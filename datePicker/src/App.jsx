99a5c8a03536261df38cb1cafd42776fc8fad452
Aayush Dhungel <adhungel0413@gmail.com>
2024-08-25 12:25:29 +0545

    datePicker project src files

---
+++ datePicker/src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react"
+import { DatePicker } from "./DatePicker"
+
+export default function App() {
+  const [value, setValue] = useState()
+  return <DatePicker value={value} onChange={setValue} />
+}
