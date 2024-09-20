99c9f23c324e62c7a28c6241b7d9f898cc7fd7ce
Aayush Dhungel <adhungel0413@gmail.com>
2024-09-20 22:28:35 +0545

    infinite scroll project

---
+++ infiniteScrollReact/client/src/App.jsx
@@ -0,0 +1,69 @@
+import { useCallback, useEffect, useRef, useState } from "react"
+import { parseLinkHeader } from "./parseLinkHeader"
+
+const LIMIT = 10
+
+export default function App() {
+  const [photos, setPhotos] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const nextPhotoUrlRef = useRef()
+
+  async function fetchPhotos(url, { overwrite = false } = {}) {
+    setIsLoading(true)
+    try {
+      await new Promise(res => setTimeout(res, 2000))
+      const res = await fetch(url)
+      nextPhotoUrlRef.current = parseLinkHeader(res.headers.get("Link")).next
+      const photos = await res.json()
+
+      if (overwrite) {
+        setPhotos(photos)
+      } else {
+        setPhotos(prevPhotos => [...prevPhotos, ...photos])
+      }
+    } catch (e) {
+      console.error(e)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const imageRef = useCallback(image => {
+    if (image == null || nextPhotoUrlRef.current == null) return
+
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        fetchPhotos(nextPhotoUrlRef.current)
+        observer.unobserve(image)
+      }
+    })
+
+    observer.observe(image)
+  }, [])
+
+  useEffect(() => {
+    fetchPhotos(`http://localhost:3000/photos?_page=1&_limit=${LIMIT}`, {
+      overwrite: true,
+    })
+  }, [])
+
+  return (
+    <div className="grid">
+      {photos.map((photo, index) => (
+        <img
+          src={photo.url}
+          key={photo.id}
+          ref={index === photos.length - 1 ? imageRef : undefined}
+        />
+      ))}
+      {isLoading &&
+        Array.from({ length: LIMIT }, (_, index) => index).map(n => {
+          return (
+            <div key={n} className="skeleton">
+              Loading...
+            </div>
+          )
+        })}
+    </div>
+  )
+}
